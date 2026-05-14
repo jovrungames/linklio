7704c621fba326a97bdd7df92a90ba382c845ac9
Adem Duran <ademduran@Adem-MacBook-Air.local>
2026-05-14 13:04:19 +0300

    fix: add backend folder

---
+++ backend/next.config.ts
@@ -0,0 +1,8 @@
+// backend/next.config.ts
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+	// Vercel için standalone gerekmez, otomatik halleder
+};
+
+export default nextConfig;
